528a21375927b8069eb7011e4767111fe3454f77
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-09-09 17:01:39 +0300

    corrected

--- taxi/admin.py
+++ taxi/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from taxi.models import Driver, Car, Manufacturer
 
+
 @admin.register(Driver)
 class AdminDriver(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
@@ -13,13 +14,13 @@ class AdminDriver(UserAdmin):
     list_display = ("username", "first_name", "last_name", "license_number")
     search_fields = ("username", "first_name", "last_name", "license_number")
 
+
 @admin.register(Car)
 class AdminCar(admin.ModelAdmin):
     list_display = ("model", "manufacturer")
-
     search_fields = ("model",)
-
     list_filter = ("manufacturer",)
+
 
 @admin.register(Manufacturer)
 class AdminManufacturer(admin.ModelAdmin):
